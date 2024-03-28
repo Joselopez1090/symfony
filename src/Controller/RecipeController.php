77174620b4eae62c15901e51fba3770163df8ffa
José <pepitolopez97gmail.com>
2024-03-28 08:48:22 +0100

    cours symfony

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/recette',name: 'app_recipe_index')]
+    public function index(Request $request): Response
+    {
+        return $this->render('recipe/index.html.twig');
+    }
+
+    #[Route('/recette/{slug}-{id}', name: 'app_recipe_show', requirements : ['id'=> '\d+','slug'=>'[a-z0-9-]+'])]
+    public function show(Request $request, string $slug, int $id): Response{
+
+    //dd($slug,$id);
+    
+        //affiche normal
+       return $this->render('recipe/show.html.twig',['slug'=> $slug,
+    'id'=> $id
+    ]);
+
+    //affiche Json
+        // return new JsonResponse([
+        //   'id' => $id,
+        // 'slug' => $slug
+        //]);
+    } 
+}
